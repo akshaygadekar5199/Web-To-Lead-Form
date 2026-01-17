8b1720c9e147dd5362d5103e78a5de564464a928
akshaygadekar5199 <akshaygadekar5199@gmail.com>
2026-01-17 22:31:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforeSubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate", inputdate.value);
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+}
